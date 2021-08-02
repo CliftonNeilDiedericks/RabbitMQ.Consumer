a69e97510db90b59955b286d7a7cc846ee32416a
Clifton Neil Diedericks <007Neilo@gmail.com>
2021-08-02 23:57:15 +0200

    multi consumer

--- Program.cs
+++ Program.cs
@@ -17,23 +17,7 @@ namespace RabbitMQ.Consumer
             };
             using var connection = factory.CreateConnection();
             using var channel = connection.CreateModel();
-            channel.QueueDeclare("demo-queue",
-                durable: true,
-                exclusive: false,
-                autoDelete: false,
-                arguments: null
-                );
-
-            var consumer = new EventingBasicConsumer(channel);
-            consumer.Received += (sender, e) =>
-            {
-                var body = e.Body.ToArray();
-                var message = Encoding.UTF8.GetString(body);
-                Console.WriteLine(message);
-            };
-
-            channel.BasicConsume("demo-queue",true,consumer);
-            Console.ReadLine();
+            QueueConsumer.Consume(channel);
         }
     }
 
